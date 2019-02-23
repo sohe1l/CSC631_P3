ed273fb399706468171e23c96cea202d15a47e45
sohe1l <soheilansari@icloud.com>
2019-02-23 14:53:00 -0800

    Added creating objects with clicks

---
+++ Assets/Scripts/CreatingObjects.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatingObjects : MonoBehaviour
+{
+
+    public GameObject prefab;
+    Ray ray;
+    RaycastHit hit;
+
+
+    public Camera firstPersonCamera;
+    public Camera overheadCamera;
+
+    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.Mouse0))
+        //{
+        //    createObject();
+        //}
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            createObject(Input.mousePosition);
+        }
+
+    }
+
+
+    public void createObject(Vector2 mousePosition)
+    {
+        RaycastHit hit = RayFromCamera(mousePosition, 1000.0f);
+        GameObject.Instantiate(prefab, hit.point, Quaternion.identity);
+    }
+
+    public RaycastHit RayFromCamera(Vector3 mousePosition, float rayLength)
+    {
+        RaycastHit hit; 
+        Ray ray = getCurrentCamera().ScreenPointToRay(mousePosition);
+        Physics.Raycast(ray, out hit, rayLength);
+        return hit;
+    }
+
+    private Camera getCurrentCamera()
+    {
+        if (firstPersonCamera.enabled)
+        {
+            return firstPersonCamera;
+        }
+        else
+        {
+            return overheadCamera;
+        }
+    }
+
+    //private void createObject()
+    //{
+    //    if (Physics.Raycast(ray, out hit))
+    //    {
+    //        Debug.Log("Creatingggggg - yes ");
+    //        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        GameObject obj = Instantiate(prefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Creatingggggg - no ");
+    //    }
+    //}
+
+
+}
