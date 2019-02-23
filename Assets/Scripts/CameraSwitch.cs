dcd2e276f1b7bc078117a2874065471d6f1402f3
sohe1l <soheilansari@icloud.com>
2019-02-22 21:26:34 -0800

    initial commit

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+    public Camera firstPersonCamera;
+    public Camera overheadCamera;
+
+    bool isFirstPerson;
+        void Start()
+    {
+        ShowFirstPersonView();
+        isFirstPerson = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (isFirstPerson)
+            {
+                ShowOverheadView();
+            }
+            else
+            {
+                ShowFirstPersonView();
+            }
+            isFirstPerson = !isFirstPerson;
+        }
+    }
+
+    public void ShowOverheadView()
+    {
+        firstPersonCamera.enabled = false;
+        overheadCamera.enabled = true;
+    }
+
+    public void ShowFirstPersonView()
+    {
+        firstPersonCamera.enabled = true;
+        overheadCamera.enabled = false;
+    }
+}
---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public void GotoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void GotoScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void GotoScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
