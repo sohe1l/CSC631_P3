0b81cb39199ead6dc8719a90d2fcd5e0d7b238b0
sohe1l <soheilansari@icloud.com>
2019-02-22 22:06:08 -0800

    Added day night effect

---
+++ Assets/Scripts/SceneHelper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHelper : MonoBehaviour
+{
+
+    public Light scene_light;
+    bool isDay = true;
+
+    public void GotoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void GotoScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void GotoScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void SwitchNight()
+    {
+        if (isDay)
+        {
+            scene_light.intensity = 0.2F;
+        }
+        else
+        {
+            scene_light.intensity = 1F;
+        }
+        isDay = !isDay;
+    }
+}
